7d8c2ec6095446f0528657a4209f695a60a7fc14
Jay S <jays@Jays-MacBook-Air.local>
2026-05-15 08:11:28 +0530

    Rebuild with Vite: build step outputs dist/, Express serves dist/ in prod

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.post('/api/contact', require('./api/contact'));
 app.post('/api/quote', require('./api/quote'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
